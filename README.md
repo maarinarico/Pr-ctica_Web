d6bd6da25c79e58c4a2a14782d13679908af6898
Marina Rico <maarinarico@gmail.com>
2018-03-06 18:49:06 +0100

    Termino la practica web

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pasos a seguir:
+
+###  1. Descargar este repositorio.
+###  2. Abrir el proyecto con Visual Studio Code.
+### 3. Darle a gulp para abrir la web.
---
+++ src/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var notify = require("gulp-notify");
+var browserSync = require("browser-sync").create();
+var gulpImport = require("gulp-html-import");
+//Para optimizar las imágenes
+var imagemin = require("gulp-imagemin");
+
+gulp.task("default", ["img", "html", "sass"], function(){
+    browserSync.init({ server: "dist/" });
+    gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["sass"]);
+    gulp.watch(["src/*.html", "src/**/*.html"], ["html"]);
+
+});
+
+gulp.task("sass", function(){
+    gulp.src("src/scss/style.scss")
+        .pipe(sass().on("error", function(error){ 
+            // return notify().write(error); 
+        }))
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream())
+        // .pipe(notify("SASS Compilado OK”))
+});
+
+gulp.task("html", function(){
+    gulp.src("src/*.html")
+        .pipe(gulpImport("src/components/"))
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream())
+        // .pipe(notify("HTML importado"));
+});
+
+// tarea que optimiza las imágenes
+// Cogemos las imágenes de "src/img/*", optimizamos su peso y las llevo a "dist/img/"
+gulp.task("img", function(){
+    gulp.src("src/img/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img/"))
+
+});
